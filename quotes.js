158cc057072c7f2dbe5874a91eb3ec273ef97cf5
JamesAManson <jamesanmanson@gmail.com>
2021-06-14 11:57:11 +1000

    Create quotes.js
    Initial quotes added

---
+++ quotes.js
@@ -0,0 +1,2 @@
+const quotes = ['The strength of the team is each individual member. The strength of each member is the team. - Phil Jackson', 'Everyone has the fire, but the champions know when to ignite the spark. - Amit Ray', 'One man can be a crucial ingredient on a team, but one man cannot make a team. - Kareem Abdul Jabbar']
+
